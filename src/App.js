a3be1f31c0b1c2a18bf005f952b5915965b2faf3
Casto2710 <84015996+Casto2710@users.noreply.github.com>
2021-10-07 15:30:08 +0100

    added dictionary search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Dictionary from "./Dictionary"
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo 
         img-fluid" alt="logo" />
-        <h1>Dictionary App</h1>
       </header>
-     
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Jolanta Studencka </small>
+        </footer>
+     </div>
     </div>
   );
 }
