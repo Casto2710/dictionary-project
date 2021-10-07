e28f39b69e993d652dbbd3e65f548077a38e21de
Casto2710 <84015996+Casto2710@users.noreply.github.com>
2021-10-07 13:51:39 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo 
+        img-fluid" alt="logo" />
+        <h1>Dictionary App</h1>
+      </header>
+     
+    </div>
+  );
+}
+
+
