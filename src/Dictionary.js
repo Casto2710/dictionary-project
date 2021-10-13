a061e2a7ec24d781d38da651e85c598e49075338
Casto2710 <84015996+Casto2710@users.noreply.github.com>
2021-10-13 18:24:15 +0100

    added styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -35,7 +35,9 @@ export default function Dictionary(props) {
     return (
         <div className="Dictionary">
      <section>
-       <h1>Dictionary</h1>
+     <h1>Dictionary</h1>
+     What do you want to look up?
+     <br />
       <form onSubmit={handleSubmit}>
           <input 
           type="search"
